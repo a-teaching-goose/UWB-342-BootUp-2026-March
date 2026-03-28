13b6b4797debb84135057f7cdb1985853c6ff28f
Peng Du <rick.peng.du@gmail.com>
2026-03-28 09:05:56 -0700

    templated queue and message queue

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 
+template<typename T>
 class Queue {
 private:
     uint capacity;
     uint size;
-    int *data;
+    T *data;
 
     uint front, end;
 
 public:
     Queue(const uint capacity) : size(0), front(0), end(0) {
         this->capacity = capacity;
-        this->data = new int[capacity];
+        this->data = new T[capacity];
     }
 
     // end....front
-    bool enqueue(const int value) {
+    bool enqueue(const T value) {
         if (size == capacity) {
             std::cout << "Queue is full" << std::endl;
             return false;
@@ -26,7 +27,7 @@ public:
         return true;
     }
 
-    bool dequeue(int &value) {
+    bool dequeue(T &value) {
         if (isEmpty()) {
             std::cout << "Queue is empty" << std::endl;
             return false;
@@ -38,17 +39,27 @@ public:
         return true;
     }
 
-    bool isEmpty() const {
+    [[nodiscard]] bool isEmpty() const {
         return size == 0;
     }
 
     ~Queue() {
         delete[] data;
     }
+
+    std::string to_string() {
+        std::string result;
+        for (uint i = 0; i < size; ++i) {
+            result += "data[" + std::to_string(i) + "]: " + data[front + i] + "\n";
+        }
+        return result;
+    }
 };
 
+typedef Queue<std::string> MessageQueue;
+
 int main() {
-    Queue queue(5);
+    Queue<int> queue(5);
     for (int i = 0; i < 5; ++i) {
         queue.enqueue(i);
     }
@@ -62,4 +73,11 @@ int main() {
         }
         std::cout << val << std::endl;
     }
+
+    MessageQueue messageQueue(10);
+    for (int i = 0; i < 10; ++i) {
+        messageQueue.enqueue("Hello world: " + std::to_string(i));
+    }
+
+    std::cout << messageQueue.to_string() << std::endl;
 }
