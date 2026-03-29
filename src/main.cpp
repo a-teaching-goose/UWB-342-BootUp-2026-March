0b3086e7d129cdf54982964ca47ab73284a1c8f7
Peng Du <rick.peng.du@gmail.com>
2026-03-29 15:11:53 -0700

    class demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+
 int main() {
-    std::cout << "Hello C++ BootUp!" << std::endl;
-    return 0;
+    // option 1: $1M
+    // option 2: 1 cent, 2 cents, 4 cents, doubling till 30 total days
+    float ending_balance = 0.01; // 1 cent
+
+    for (int i = 0; i < 29; i++) {
+        printf("day %d: %f\n", i, ending_balance);
+        ending_balance += ending_balance;
+    }
+
+    // be aware of round-off error
+    printf("total ending balance after 30 days: %f\n", ending_balance);
 }
