13d7d7b843903299c7f6e4962083a28dec24a1f7
Peng Du <rick.peng.du@gmail.com>
2026-03-29 15:56:26 -0700

    improve readibility

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,14 @@
 int main() {
     // option 1: $1M
     // option 2: 1 cent, 2 cents, 4 cents, doubling till 30 total days
-    float ending_balance = 0.01; // 1 cent
+    float ending_balance_in_dollar = 1; // 1 cent
 
+    // magic number
     for (int i = 0; i < 29; i++) {
-        printf("day %d: %f\n", i, ending_balance);
-        ending_balance += ending_balance;
+        printf("day %d: %f\n", i, ending_balance_in_dollar);
+        ending_balance_in_dollar += ending_balance_in_dollar;
     }
 
     // be aware of round-off error
-    printf("total ending balance after 30 days: %f\n", ending_balance);
+    printf("total ending balance after 30 days: %f\n", ending_balance_in_dollar);
 }
