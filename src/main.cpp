289fb12d00b61032fe97ad8dd25efdddc8ea07f1
Peng Du <rick.peng.du@gmail.com>
2026-03-29 14:09:42 -0700

    class demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 int main() {
-    std::cout << "Hello C++ BootUp!" << std::endl;
-    return 0;
+    int my_data = 50;
+
+    std::string messages[5];
+
+    for (int i=0; i<my_data; i++) {
+        messages[i] = "Hello " + std::to_string(i);
+    }
+
+    int i = 0;
+    for (;;) {
+        if (i==my_data) {
+            break;
+        }
+        if (i%2==0) {
+            std::cout << messages[i] << std::endl;
+        }
+        i++;
+    }
 }
